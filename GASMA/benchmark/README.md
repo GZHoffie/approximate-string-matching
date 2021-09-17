41cfebfd061f8ade6be2fa14bb78522a211cb192
Zhenhao Gu <guzhenhao1@sjtu.edu.cn>
2021-09-18 05:05:04 +0800

    Update MAE and CR

--- GASMA/benchmark/README.md
+++ GASMA/benchmark/README.md
@@ -3,5 +3,5 @@
 Use the following lines to generate dataset
 
 ```c
-./GASMA/bin/generate_dataset -n 1000000 -l 100 -e 0.2 -o ./pymatch/test/resource/sample.random.dataset.seq
+./GASMA/bin/generate_dataset -n 1000000 -l 100 -e 0.3 -o ./pymatch/test/resource/sample.random.dataset.seq
 ```
--- pymatch/test/test_gs.py
+++ pymatch/test/test_gs.py
@@ -2,19 +2,19 @@ from pymatch.algorithms import GASMAProjection, NeedlemanWunsch
 import time
 
 test_file = "/home/zhenhao/approximate-string-matching/pymatch/test/resource/sample.random.dataset.seq"
-ref_file = "/home/zhenhao/approximate-string-matching/pymatch/test/resource/nw.txt"
-test_items = 10000
+ref_file = "/home/zhenhao/approximate-string-matching/pymatch/test/resource/nw2.txt"
+test_items = 50000
 
 GASMATime = 0
 NWTime = 0
 error = 0
 correct = 0
-upper_limit = 20
+upper_limit = 21
 
 errorDict = {n: 0 for n in range(upper_limit)}
 correctDict = {n: 0 for n in range(upper_limit)}
 countDict = {n: 0 for n in range(upper_limit)}
-
+closeDict = {n: 0 for n in range(upper_limit)}
 with open(ref_file, "r") as rf:
     with open(test_file, "r") as f:
         i = 0
@@ -24,6 +24,10 @@ with open(ref_file, "r") as rf:
                 break
             str1 = line[1:][:-1]
             str2 = f.readline()[1:][:-1]
+
+            NWcost = int(float(rf.readline()[:-1]))
+            if NWcost > 20:
+                continue
 
 
             currTime = time.time()
@@ -41,12 +45,13 @@ with open(ref_file, "r") as rf:
             #nw = NeedlemanWunsch(str1, str2)
             #NWcost = nw.editDistance()
             #NWTime += time.time() - currTime
-            NWcost = int(float(rf.readline()[:-1]))
+            
 
             #error += abs(cost - NWcost)
             #correct += (cost == NWcost)
             errorDict[NWcost] += abs(cost - NWcost)
             correctDict[NWcost] += (cost == NWcost)
+            closeDict[NWcost] += (abs(cost - NWcost) <= 2)
             countDict[NWcost] += 1
 
             if cost != NWcost and NWcost <= 5:
@@ -65,6 +70,7 @@ print("GASMA Time:", GASMATime)
 
 MAE = [errorDict[c] / countDict[c] for c in range(upper_limit)]
 correct_rate = [correctDict[c] / countDict[c] for c in range(upper_limit)]
+close_rate = [closeDict[c] / countDict[c] for c in range(upper_limit)]
 print(countDict)
 
 print(MAE)
@@ -72,20 +78,26 @@ print(correct_rate)
 
 import matplotlib.pyplot as plt
 plt.figure()
-plt.plot(MAE[0:18])
+plt.plot(MAE[0:upper_limit])
 plt.ylabel('MAE')
 plt.xlabel('Optimal Edit Distance')
 plt.savefig('/home/zhenhao/approximate-string-matching/pymatch/test/asset/MAE.png')
 
 plt.figure()
-plt.plot(correct_rate[0:18])
+plt.plot(correct_rate[0:upper_limit])
 plt.ylabel('Correct Rate')
 plt.xlabel('Optimal Edit Distance')
 plt.savefig('/home/zhenhao/approximate-string-matching/pymatch/test/asset/CR.png')
 
 import numpy as np
 plt.figure()
-plt.plot(np.array(MAE[0:18]) / np.array(range(18)))
+plt.plot(np.array(MAE[0:upper_limit]) / np.array(range(upper_limit)))
 plt.ylabel('Relative Error')
 plt.xlabel('Optimal Edit Distance')
 plt.savefig('/home/zhenhao/approximate-string-matching/pymatch/test/asset/CR_relative.png')
+
+plt.figure()
+plt.plot(close_rate[0:upper_limit])
+plt.ylabel('Close Rate')
+plt.xlabel('Optimal Edit Distance')
+plt.savefig('/home/zhenhao/approximate-string-matching/pymatch/test/asset/CloseRate.png')
